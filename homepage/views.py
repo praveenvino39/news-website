8ce9862d7d84a14f962e41db8239eb9bf04b1818
pv4you <praveena4e@gmail.com>
2020-07-31 12:34:56 +0530

    Fixed System Variable for Security Purpose

--- homepage/views.py
+++ homepage/views.py
@@ -7,33 +7,44 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
 from .models import SavedNew
-from django.http import HttpResponse
+import os
+
+
 # Create your views here.
 
 def homepage(request):
-    weatherdata = requests.get('http://api.openweathermap.org/data/2.5/weather?q=chennai&appid=98025f7a1edae67852b8b2a15b858486')
+    weather_map_api = os.environ.get('WM_API')
+    news_api = os.environ.get('NEWS_API')
+    weatherdata = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?q=chennai&appid={}'.format(weather_map_api))
     weatherdata = json.loads(weatherdata.text)
     weatherdata = weatherdata['main']
-    response = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=4c0a0778eb4b4e1aa06fa178a050c62f')
+    response = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey={}'.format(news_api))
     response = json.loads(response.text)
     response = response['articles']
     high = int(weatherdata['temp_min'])
     low = int(weatherdata['temp_max'])
-    high = high-273
-    low = low-273
+    high = high - 273
+    low = low - 273
     articles = []
     id = 0
     for article in response:
         article.update({'id': id})
         articles.append(article)
         id = id + 1
-    return render(request, 'homepage/homepage.html', {'content': response, 'high': high, 'low': low, 'meta_title': 'Snack Time News - Homepage', 'search_term':'homepage'})
+    return render(request, 'homepage/homepage.html',
+                  {'content': response, 'high': high, 'low': low, 'meta_title': 'Snack Time News - Homepage',
+                   'search_term': 'homepage'})
+
 
 def detail(request, news_id, search_term):
-    weatherdata = requests.get('http://api.openweathermap.org/data/2.5/weather?q=chennai&appid=98025f7a1edae67852b8b2a15b858486')
+    weather_map_api = os.environ.get('WM_API')
+    news_api = os.environ.get('NEWS_API')
+    weatherdata = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?q=chennai&appid={}'.format(weather_map_api))
     weatherdata = json.loads(weatherdata.text)
     weatherdata = weatherdata['main']
-    response = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=4c0a0778eb4b4e1aa06fa178a050c62f')
+    response = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey={}'.format(news_api))
     response = json.loads(response.text)
     response = response['articles']
     high = int(weatherdata['temp_min'])
@@ -50,7 +61,10 @@ def detail(request, news_id, search_term):
     for article in articles:
         if article['id'] == news_id:
             selected_news = article
-    return render(request, 'homepage/detail.html', {'selected_news': selected_news, 'meta_title': selected_news['title'],'id': news_id,'high':high, 'low': low,'search_term':'default'})
+    return render(request, 'homepage/detail.html',
+                  {'selected_news': selected_news, 'meta_title': selected_news['title'], 'id': news_id, 'high': high,
+                   'low': low, 'search_term': 'default'})
+
 
 def signup_user(request):
     if request.user.is_authenticated:
@@ -74,6 +88,7 @@ def signup_user(request):
                 return redirect('signup')
         return render(request, 'homepage/signup.html')
 
+
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('homepage')
@@ -81,7 +96,7 @@ def login_user(request):
         if request.method == 'POST':
             username = request.POST['username']
             password = request.POST['password']
-            user = authenticate(request,username=username, password=password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 messages.success(request, 'Hello, Now you\'ve logged in.', extra_tags='success')
@@ -91,15 +106,19 @@ def login_user(request):
                 return redirect('login')
         return render(request, 'homepage/login.html', {'meta_title': 'Snack Time News - Login'})
 
+
 def logout_user(request):
     if request.method == 'POST':
         logout(request)
         return redirect('homepage')
     else:
         return redirect('homepage')
+
+
 @login_required()
-def saved_news(request, news_id,search_term):
-    response = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=4c0a0778eb4b4e1aa06fa178a050c62f')
+def saved_news(request, news_id, search_term):
+    news_api = os.environ.get('NEWS_API')
+    response = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey={}'.format(news_api))
     response = json.loads(response.text)
     response = response['articles']
     articles = []
@@ -112,7 +131,7 @@ def saved_news(request, news_id,search_term):
     for article in articles:
         if article['id'] == news_id:
             selected_news = article
-    users=request.user.username
+    users = request.user.username
     print(type(selected_news['content']))
     if selected_news['content'] is None:
         new_save = SavedNew(user=users, title=selected_news['title'], description=selected_news['description'],
@@ -123,35 +142,48 @@ def saved_news(request, news_id,search_term):
         messages.success(request, 'News added to saved!', extra_tags='success')
         return redirect('homepage')
     else:
-        new_save = SavedNew(user=users, title=selected_news['title'],content=selected_news['content'], description = selected_news['description'], urlToImage=selected_news['urlToImage'], url=selected_news['url'], date=selected_news['publishedAt'], source=selected_news['source']['name'])
+        new_save = SavedNew(user=users, title=selected_news['title'], content=selected_news['content'],
+                            description=selected_news['description'], urlToImage=selected_news['urlToImage'],
+                            url=selected_news['url'], date=selected_news['publishedAt'],
+                            source=selected_news['source']['name'])
         new_save.save()
         messages.success(request, 'News added to saved!', extra_tags='success')
         return redirect('homepage')
+
+
 @login_required()
 def saved_list(request):
     contents = SavedNew.objects.filter(user=request.user.username)
-    return render(request, 'homepage/saved_list.html',{'content': contents,'meta_title': 'Snack Time News - Your Picks'})
+    return render(request, 'homepage/saved_list.html',
+                  {'content': contents, 'meta_title': 'Snack Time News - Your Picks'})
+
 
 @login_required()
 def saved_detail(request, id):
     selected_news = get_object_or_404(SavedNew, pk=id)
     if selected_news.user == request.user.username:
-        return render(request, 'homepage/saved_detail.html',{'selected_news': selected_news,'meta_title': selected_news.title})
+        return render(request, 'homepage/saved_detail.html',
+                      {'selected_news': selected_news, 'meta_title': selected_news.title})
     else:
         return redirect('homepage')
+
 
 def delete(request, id):
     news = get_object_or_404(SavedNew, pk=id)
     news.delete()
     return redirect('saved_list')
 
+
 def search(request):
+    weather_map_api = os.environ.get('WM_API')
+    news_api = os.environ.get('NEWS_API')
     if request.method == 'POST':
-        weatherdata = requests.get('http://api.openweathermap.org/data/2.5/weather?q=chennai&appid=98025f7a1edae67852b8b2a15b858486')
+        weatherdata = requests.get(
+            'http://api.openweathermap.org/data/2.5/weather?q=chennai&appid={}'.format(weather_map_api))
         weatherdata = json.loads(weatherdata.text)
         weatherdata = weatherdata['main']
         search_term = request.POST['search']
-        url = 'https://newsapi.org/v2/everything?q={}&apiKey=4c0a0778eb4b4e1aa06fa178a050c62f'.format(search_term)
+        url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(search_term, news_api)
         response = requests.get(url)
         response = json.loads(response.text)
         response = response['articles']
@@ -166,15 +198,21 @@ def search(request):
             articles.append(article)
             id = id + 1
         return render(request, 'homepage/search.html',
-                      {'content': response, 'high': high, 'low': low,'search_term': request.POST["search"],'search_term': search_term, 'meta_title': 'Snack Time News - {}'.format(search_term)})
+                      {'content': response, 'high': high, 'low': low, 'search_term': request.POST["search"],
+                       'search_term': search_term, 'meta_title': 'Snack Time News - {}'.format(search_term)})
     else:
         return redirect('homepage')
-def search_detail(request, news_id,search_term):
+
+
+def search_detail(request, news_id, search_term):
+    weather_map_api = os.environ.get('WM_API')
+    news_api = os.environ.get('NEWS_API')
     print(search_term)
-    weatherdata = requests.get('http://api.openweathermap.org/data/2.5/weather?q=chennai&appid=98025f7a1edae67852b8b2a15b858486')
+    weatherdata = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?q=chennai&appid={}'.format(weather_map_api))
     weatherdata = json.loads(weatherdata.text)
     weatherdata = weatherdata['main']
-    url = 'https://newsapi.org/v2/everything?q={}&apiKey=4c0a0778eb4b4e1aa06fa178a050c62f'.format(search_term)
+    url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(search_term, news_api)
     response = requests.get(url)
     response = json.loads(response.text)
     response = response['articles']
@@ -192,11 +230,16 @@ def search_detail(request, news_id,search_term):
     for article in articles:
         if article['id'] == news_id:
             selected_news = article
-    return render(request, 'homepage/detail.html', {'selected_news': selected_news, 'meta_title': selected_news['title'],'id': news_id,'high':high, 'low': low,'search_term':search_term})
+    return render(request, 'homepage/detail.html',
+                  {'selected_news': selected_news, 'meta_title': selected_news['title'], 'id': news_id, 'high': high,
+                   'low': low, 'search_term': search_term})
+
 
 @login_required()
 def saved_news_search(request, news_id, search_term):
-    url= 'https://newsapi.org/v2/everything?q={}&apiKey=4c0a0778eb4b4e1aa06fa178a050c62f'.format(search_term)
+    weather_map_api = os.environ.get('WM_API')
+    news_api = os.environ.get('NEWS_API')
+    url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(search_term, news_api)
     response = requests.get(url)
     response = json.loads(response.text)
     response = response['articles']
@@ -210,7 +253,7 @@ def saved_news_search(request, news_id, search_term):
     for article in articles:
         if article['id'] == news_id:
             selected_news = article
-    users=request.user.username
+    users = request.user.username
     print(type(selected_news['content']))
     if selected_news['content'] is None:
         new_save = SavedNew(user=users, title=selected_news['title'], description=selected_news['description'],
@@ -221,7 +264,10 @@ def saved_news_search(request, news_id, search_term):
         messages.success(request, 'News added to saved!', extra_tags='success')
         return redirect('homepage')
     else:
-        new_save = SavedNew(user=users, title=selected_news['title'],content=selected_news['content'], description = selected_news['description'], urlToImage=selected_news['urlToImage'], url=selected_news['url'], date=selected_news['publishedAt'], source=selected_news['source']['name'])
+        new_save = SavedNew(user=users, title=selected_news['title'], content=selected_news['content'],
+                            description=selected_news['description'], urlToImage=selected_news['urlToImage'],
+                            url=selected_news['url'], date=selected_news['publishedAt'],
+                            source=selected_news['source']['name'])
         new_save.save()
         messages.success(request, 'News added to saved!', extra_tags='success')
         return redirect('homepage')
--- newsapp/settings.py
+++ newsapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7w4$%4(7ki6g81d@!6u0!=)7z&ot2_=zcigc2$0d8je^^)_a68'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
